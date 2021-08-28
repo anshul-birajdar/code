79a29a0076533b4b0a61cddca62b4a636d8e46bc
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-08-28 11:26:02 +0530

    autoupdate 2021-08-28-11:26:01

---
+++ prog_lang/lang.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define MAXBUF 1000
+int main(void)
+{
+	char buf[MAXBUF];
+	fgets(buf,MAXBUF,stdout);
+	//scanf("%s",&buf);
+	char commands[2][MAXBUF];
+	int index=0;
+	int loc=0;
+	int bufloc=0;
+	while(1)
+	{
+		if(buf[bufloc]==' ')
+		{
+			loc++;
+			bufloc=0;
+			index++;
+			continue;
+		}
+		if(buf[bufloc]=='\0')break;
+		commands[index][loc]=buf[bufloc];
+		loc++;
+		bufloc++;
+	}
+	printf("%s\n%s\n",buf[0],buf[1]);
+}
+	
+	
+	
