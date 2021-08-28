f2dcf245c7e300d1f673920448ea792ca9cb046c
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-08-28 10:41:01 +0530

    autoupdate 2021-08-28-10:41:01

---
+++ prog_lang/separate.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(void)
+{
+	char input[1000];
+	char *commands[5][1000];
+	printf("]");
+	scanf("%[^\n]s",input);
+	separate(input,&commands);
+	for(int i=0;commands[i]!=NULL;i++)
+		printf("%s\n",commands[i]);
+}
+	
+void separate(char input[],char *commands[][])
+{
+	int index,i;
+	for(i=0;input[i]!=NULL;i++)
+	{
+		if(input[i]==' ')
+		{
+			*(commands)[index][i]=NULL;
+			index++;
+		}
+		*(commands)[index][i]=input[i];
+	}
+	*(commands)[index][i]=NULL;
+	*(commands)[index][i+1]=NULL;
+	*(commands)[index+1]=NULL;
+	return;
+}
+
+		
+
