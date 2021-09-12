1a65aa0fe9ca2aa669e229aedc4bce8bc00dc84c
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-12 14:05:01 +0530

    autoupdate 2021-09-12-14:05:01

--- c/bcd-lang/bcd.c
+++ c/bcd-lang/bcd.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 #include"strfun.h"
 #define MAXLINE 1000
 int main(void)
@@ -9,6 +11,8 @@ int main(void)
 		printf("INPUT>");
 		fgets(line,MAXLINE,stdin);
 		bcd_rem_newline(line);
+		if(bcd_getif_eof(line)==true)
+			exit(EXIT_SUCCESS);
 		printf("%s\n",line);
 	}
 	return 0;
