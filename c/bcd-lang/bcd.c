12f6a26fb941abd3d82f499152cedd0c39eb22e6
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-12 14:56:02 +0530

    autoupdate 2021-09-12-14:56:01

--- c/bcd-lang/bcd.c
+++ c/bcd-lang/bcd.c
@@ -3,13 +3,35 @@
 #include<stdbool.h>
 #include"strfun.h"
 #define MAXLINE 1000
+#define MAXTOKEN 2
 int main(void)
 {
-	char c;
+	char *c;
 	char line[MAXLINE];
+	char tokens[MAXTOKEN][MAXLINE];
+	int currentoken=0;
+	int currentchar=0;
+	int ntokens=0;	
 	for(;;)
 	{
 		if(bcd_getline(line,MAXLINE)==true||*line=='\0')continue;
+		c=line;
+		while(1)
+		{
+			if(*c=='\n')
+			{
+				currentoken++;
+				currentchar=0;
+				c++;
+			}
+			else if(*c=='\0')
+			{
+				ntokens=currentoken;
+				break;
+			}
+			else tokens[currentoken][currentchar++]=*c++;
+			
+		}		
 		printf("%s\n",line);
 	}
 	return 0;
