48bd59f318f87a2e2a77d932b9995e1eab9ac00b
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-12 16:09:01 +0530

    autoupdate 2021-09-12-16:09:01

--- c/bcd-lang/code.py
+++ c/bcd-lang/code.py
@@ -7,7 +7,7 @@ while True:
 	if t=="print":print(acc)
 	if t=="store":acc=int(tokens[1])
 	if t=="add":acc+=int(tokens[1])
-	if t[-1]:lines[t[:-1]]=tokens[1:]
-	print(lines,type(line))	
+	if t[-1]==":":lines[t[:-1]]=tokens[1:]
+	print(lines,type(lines))	
 		
 
