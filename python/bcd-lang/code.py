5f9dfcd62c01b52dc7d611dbf42c37ac9b84851f
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-19 16:20:01 +0530

    autoupdate 2021-09-19-16:20:01

--- python/bcd-lang/code.py
+++ python/bcd-lang/code.py
@@ -12,7 +12,8 @@ def exec(lines):
 			i+=1
 			line=lines[i]
 			c=line[0]
-			if c=="add":acc+=float(line[1])
+			if c=="addi":acc+=float(line[1])
+			if c=="add":acc+=float(mem[line[1]])
 			if c=="print":print(acc)
 			if c=="load":acc=mem[int(line[1])]
 			if c=="ldi":acc=float(line[1])
