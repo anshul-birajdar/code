00ea6c1e6d517701060170fcd2c8d5964fcfa92d
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-19 13:32:02 +0530

    autoupdate 2021-09-19-13:32:02

--- python/bcd-lang/code.py
+++ python/bcd-lang/code.py
@@ -1,22 +1,14 @@
-acc=0;
-lines={}
-def exec(command):
-	global acc,lines
-	tokens=val.split()
-	t=tokens[0]
-	if t=="do":pass
-	if t=="print":print(acc)
-	if t=="store":acc=int(tokens[1])
-	if t=="add":acc+=int(tokens[1])
-	if t[-1]==":":lines[t[:-1]]=tokens[1] # 1:
-	if t=="run" and t!=[]:
-		tokens=tokens[1:]
-		print(tokens)
-		for line in lines:
-			exec(lines[line])
-while True:
-	val=input(">");
-	exec(val);
-	print(lines,type(lines))	
+def exec(lines):print(lines)
+
+def main():
+	lines = []
+	while True:
+		line=input()
+		if(line=="run"):exec(lines)
+		else:
+			lines.append(line.split())
+if(__name__=="__main__"):
+	main()
+	
 		
 
