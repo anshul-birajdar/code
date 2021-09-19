af10c15fb2055cf59422e0d0b5409820d861f6ed
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-19 13:45:02 +0530

    autoupdate 2021-09-19-13:45:01

--- python/bcd-lang/code.py
+++ python/bcd-lang/code.py
@@ -2,7 +2,7 @@ def exec(lines):print(lines)
 def convert(lines):
 	for i in range(0,len(lines)-1):
 		print("--I LOOP--",i)
-		for j in range(0,len(lines[i])-1):
+		for j in range(0,len(lines[i])):
 			print("---J LOOP---",j)
 			#try:
 			lines[i][j]=float(lines[i][j])
