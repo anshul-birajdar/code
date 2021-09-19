8ac4cb9e36284997b9c0565e665429a59b3083cb
Anshul Birajdar <linux.forall.yt@gmail.com>
2021-09-19 13:37:01 +0530

    autoupdate 2021-09-19-13:37:01

--- python/bcd-lang/code.py
+++ python/bcd-lang/code.py
@@ -1,5 +1,12 @@
 def exec(lines):print(lines)
-
+def convert(lines):
+	for i in range(0,len(lines)-1):
+		for j in range(0,len(lines[i])-1):
+			try:
+				lines[i][j]=float(lines[i][j])
+			except:
+				pass
+			
 def main():
 	lines = []
 	while True:
@@ -7,6 +14,7 @@ def main():
 		if(line=="run"):exec(lines)
 		else:
 			lines.append(line.split())
+			lines=convert(lines)
 if(__name__=="__main__"):
 	main()
 	
