82ee5ad017fcecfb4312ffc983a82ca46fe7e2ae
anshul-birajdar <linux.forall.yt@gmail.com>
2021-09-30 19:29:01 +0530

    autoupdate 2021-09-30-19:29:01

--- c/mnply/mnply.c
+++ c/mnply/mnply.c
@@ -2,16 +2,20 @@
 #include<time.h>
 #include<stdlib.h>
 #include"mnply.h"
+#define MAXNAME 8+1
 
 int main(void)
 {
-	while(1)
+	//while(1)
 	{
-		int start,end;
-		start=10;
-		end=20;
-		srand(time(NULL));
-		//printf("%d\n",rand()%(end-start)+start);
+		int players=mnply_getint("Enter the Number of Players\n");
+		char **names=malloc(players*sizeof(char *));
+		for(int i=0;i<players;i++)
+		{
+			names[i]=malloc(MAXNAME*sizeof(char));
+			fgets(names[i],MAXNAME-1,stdin);
+			printf("Your name is:%s\n",names[i]);
+		}
 		printf("%d\n",mnply_rolldice());
 	}
 }
