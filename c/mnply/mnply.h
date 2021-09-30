a43e17b8195a608f1ce1dc9228618a7df564df83
anshul-birajdar <linux.forall.yt@gmail.com>
2021-09-30 21:46:01 +0530

    autoupdate 2021-09-30-21:46:01

--- c/mnply/mnply.h
+++ c/mnply/mnply.h
@@ -19,7 +19,8 @@ int mnply_getint(char *message)
 void mnply_getnames(int players,char ***pnames,int maxname)
 {
 	printf("Enter Name [ 0] :");
-	fgets((*pnames)[0],maxname-1,stdin);
+	//fgets((*pnames)[0],maxname-1,stdin);
+	scanf("%s",*(pnames)[0]);
         for(int i=1;i<players;i++)
         {
                 (*pnames)[i]=malloc(maxname*sizeof(char));
