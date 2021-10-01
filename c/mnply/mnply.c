c7b9da31b32beb08a1463024bf7b4eaed0961d1d
anshul-birajdar <linux.forall.yt@gmail.com>
2021-10-01 09:44:01 +0530

    autoupdate 2021-10-01-09:44:01

--- c/mnply/mnply.c
+++ c/mnply/mnply.c
@@ -9,4 +9,13 @@ int main(void)
 	int players=mnply_getint("Enter the Number of Players: ");
 	char **names=malloc(players*sizeof(char *));
 	mnply_getnames(players,&names,MAXNAME);
+	while(1)
+	{
+		for(int i=0;i<players;i++)
+		{
+			printf("Player:\t%s\n");
+			printf("Dice:\t%d\n",mnply_rolldice()+mnply_rolldice());
+			getchar(stdin);
+		}
+	}
 }
