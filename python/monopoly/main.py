b7112169b39becf25a9c4010c458ce2fef165cf6
anshul-birajdar <linux.forall.yt@gmail.com>
2021-10-01 10:22:01 +0530

    autoupdate 2021-10-01-10:22:01

--- python/monopoly/main.py
+++ python/monopoly/main.py
@@ -6,6 +6,22 @@ with open("/usr/share/dict/cracklib-small") as f:
 name=random.choice(wordlist)
 while len(name)>10 or name.lower()!=name or len(name)<6:
 	name=random.choice(wordlist)
-print(name)
+namec=[char for char in name]
+gname=namec
+for i in range(0,len(namec)):
+	if namec[i] not in "aeiou":
+		namec[i]='_'
+turns=0
+while ''.join(namec) != name and turns>6:
+	turns+=1
+	if turns>4:break
+	print(''.join(namec))
+	char=input(">");
+	char=char[0]
+	for i in range(0,len(gname)):
+		if gname[i]==char:
+			cname[i]=char
+		
 	
+		
 
