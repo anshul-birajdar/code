1c8949563e09a06661027fa7bd0bf9be4b84956e
anshul-birajdar <linux.forall.yt@gmail.com>
2021-10-01 10:10:01 +0530

    autoupdate 2021-10-01-10:10:01

--- python/monopoly/main.py
+++ python/monopoly/main.py
@@ -4,7 +4,7 @@ with open("/usr/share/dict/cracklib-small") as f:
 	for line in f:
 		wordlist.append(line.strip())
 name=random.choice(wordlist)
-while len(name)>6 and name.lower()!=name:
+while len(name)>10 or name.lower()!=name or len(name)<6:
 	name=random.choice(wordlist)
 print(name)
 	
