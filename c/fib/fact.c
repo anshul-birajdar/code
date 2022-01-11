3984589e15f3e7296aa567a9c2c46286e02c1b69
anshul-birajdar <linux.forall.yt@gmail.com>
2022-01-11 13:51:01 +0530

    autoupdate 2022-01-11-13:51:01

--- c/fib/fact.c
+++ c/fib/fact.c
@@ -1,10 +1,9 @@
-#define MAX 7
 #include<stdio.h>
 #include<stdlib.h>
 int fact(n)
 int n;
 {
-	printf("FACT:%d\t%p\n",n,&n);
+	//printf("FACT:%d\t%p\n",n,&n);
 	if(n==1)return 1;
 	return  n * fact(n-1);
 }
