f50ae12dc88cc303e9ac4f7cb928e45bc4487689
anshul-birajdar <linux.forall.yt@gmail.com>
2022-01-11 13:48:02 +0530

    autoupdate 2022-01-11-13:48:02

--- c/fib/fact.c
+++ c/fib/fact.c
@@ -1,18 +1,19 @@
+#define MAX 7
 #include<stdio.h>
 int fact(n)
 int n;
 {
 	printf("FACT:%d\t%p\n",n,&n);
 	if(n==1)return 1;
-	return fact(n-1);
+	return  n * fact(n-1);
 }
-main()
+main(argc,argv)
+int argc;
+char **argv;
 {
 	printf("no.	value\n");
-	printf("%d	%d\n",1,fact(1));
-	printf("%d	%d\n",2,fact(2));
-	printf("%d	%d\n",3,fact(3));
-	printf("%d	%d\n",4,fact(4));
-	printf("%d	%d\n",5,fact(5));
+	int i;
+	for(i=1;i<=MAX;i++)
+		printf("%d	%d\n",i,fact(i));
 	return 0;
 }
