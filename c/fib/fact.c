ce13c7ff73145d07ce962929ea935559a513c104
anshul-birajdar <linux.forall.yt@gmail.com>
2022-01-11 13:39:02 +0530

    autoupdate 2022-01-11-13:39:02

--- c/fib/fact.c
+++ c/fib/fact.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-int fib(n)
+int fact(n)
 int n;
 {
-	printf("FIB:%d\t%p\n",n,&n);
+	printf("FACT:%d\t%p\n",n,&n);
 	if(n==1)return 1;
-	if(n==2)return 1;
-	return fib(n-1) + fib(n-2);
+	return fact(n-1);
 }
 main()
 {
 	printf("no.	value\n");
-	printf("%d	%d\n",1,fib(1));
-	printf("%d	%d\n",2,fib(2));
-	printf("%d	%d\n",3,fib(3));
-	printf("%d	%d\n",4,fib(4));
-	printf("%d	%d\n",5,fib(5));
+	printf("%d	%d\n",1,fact(1));
+	printf("%d	%d\n",2,fact(2));
+	printf("%d	%d\n",3,fact(3));
+	printf("%d	%d\n",4,fact(4));
+	printf("%d	%d\n",5,fact(5));
 	return 0;
 }
