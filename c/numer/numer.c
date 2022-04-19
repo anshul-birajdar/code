3715ac484767607a982bd3e06310d10c045f4e39
anshul-birajdar <linux.forall.yt@gmail.com>
2022-04-19 11:06:01 +0530

    autoupdate 2022-04-19-11:06:01

---
+++ c/numer/numer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAXLINE 10
+int main(void)
+{
+//while(1) {
+	char line[MAXLINE];
+	char newline[MAXLINE];
+	fgets(line,MAXLINE,stdin);
+	int len = strlen(line);
+
+	for(int i=0;line[i]!='\0';i++) {
+		printf("%c",line[i]);
+		if(len%3==1&&i%3==2)printf(",");
+		if(len%3==2&&i%3==0)printf(",");
+		if(len%3==0&&i%3==1)printf(",");
+	}
+//}
+}
