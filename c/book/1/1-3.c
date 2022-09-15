8f2da9987e521670fe699fcdfa0bb7a046be5375
anshul-birajdar <linux.forall.yt@gmail.com>
2022-09-15 20:49:02 +0530

    autoupdate 2022-09-15-20:49:01

--- c/book/1/1-3.c
+++ c/book/1/1-3.c
@@ -6,9 +6,10 @@ int main(void) {
 	upper=300;
 	step=20;
 	fahr=lower;
-	while(fahr<=upper) {
+	//while(fahr<=upper) {
+	for(int i=0;i<=300;i=i+20)
 		celsius=(5.0/9.0)*(fahr-32.0);
 		printf("%3.0f %6.1f\n",fahr,celsius);
-		fahr=fahr+step;
+		//fahr=fahr+step;
 	}
 }
