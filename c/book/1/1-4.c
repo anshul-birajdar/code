ab6fdbb140204cc95bbebff43695ecb26a25c1fd
anshul-birajdar <linux.forall.yt@gmail.com>
2022-09-19 21:11:01 +0530

    autoupdate 2022-09-19-21:11:01

--- c/book/1/1-4.c
+++ c/book/1/1-4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void) {
 	printf("%4s %7s\n","cel","fahr");
-	int celsius, fahr;
+	int fahr;
 	for(int i=-20;i<=150;i=i+=10) {
 		fahr=(9*i)/5+32;
 		printf("%4d %7d\n",i,fahr);
