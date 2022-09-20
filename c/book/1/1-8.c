6e551a6d1693d13a5eabd7226d895bca1b645a0c
anshul-birajdar <linux.forall.yt@gmail.com>
2022-09-20 21:00:01 +0530

    autoupdate 2022-09-20-21:00:01

--- c/book/1/1-8.c
+++ c/book/1/1-8.c
@@ -3,8 +3,7 @@
 int main(void) {
 	int c,b,t,nl;
 	c=b=t=nl=0;
-	while((c=getchar()!=EOF)){
-		printf(":%c:",c);
+	while((c=getchar())!=EOF){
 		if(c==' ')b++;
 		else if(c=='\t')t++;
 		else if(c=='\n')nl++;
