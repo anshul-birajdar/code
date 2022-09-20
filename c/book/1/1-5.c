e1cf6337b3bc7a4c929e80467814e22da3d57419
anshul-birajdar <linux.forall.yt@gmail.com>
2022-09-20 20:47:02 +0530

    autoupdate 2022-09-20-20:47:01

--- c/book/1/1-5.c
+++ c/book/1/1-5.c
@@ -2,7 +2,7 @@
 int main(void) {
 	printf("%4s %7s\n","fahr","celsius");
 	float fahr, celsius;
-	for(float i=0;i<=300;i=i+20) {
+	for(float i=300;i>=0;i=i-20) {
 		celsius=(5.0/9.0)*(i-32.0);
 		printf("%4.0f %7.1f\n",i,celsius);
 	}
