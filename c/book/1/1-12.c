432eb3d689a321341d5d982570f84893f9e21670
anshul-birajdar <linux.forall.yt@gmail.com>
2022-09-21 19:13:01 +0530

    autoupdate 2022-09-21-19:13:01

--- c/book/1/1-12.c
+++ c/book/1/1-12.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-	char c;
+	char c,pc;
+	pc='\0';
 	while((c=getchar())!=EOF) {
 		if(c==' ')c='\n';
 		if(c=='\t')c='\n';
+		if(c=='\n'&&pc=='\n')continue;
 		putchar(c);
+		pc=c;
 	}
 }
