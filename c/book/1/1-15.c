7b5fe1018e81924ed36f009d349f3e0251c841af
anshul-birajdar <linux.forall.yt@gmail.com>
2022-09-22 19:49:01 +0530

    autoupdate 2022-09-22-19:49:01

--- c/book/1/1-15.c
+++ c/book/1/1-15.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
-int main(void) {
+int main(void)
+{
 	printf("%4s %7s\n","fahr","celsius");
 	float fahr, celsius;
 	for(int i=0;i<=300;i=i+20) {
-		celsius=(5.0/9.0)*(i-32.0);
+		//celsius=(5.0/9.0)*(i-32.0);
+		celsius=ftoc(i);
 		printf("%4d %7.1f\n",i,celsius);
 	}
+}
+int ftoc(float f) {
+	return ((5.0/9.0)*(f-32.0));
 }
