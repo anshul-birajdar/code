01fef583e7b8f08b6efaeea79bfdbf2ccf8c90f1
anshul-birajdar <linux.forall.yt@gmail.com>
2022-10-10 21:44:01 +0530

    autoupdate 2022-10-10-21:44:01

---
+++ c/book/1/1-16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define MAXLINE 1000
+int main(void)
+{
+	int len=max=0;
+	char c='\0';
+	while(c!=EOF) {
+		for(len=0;(c=getchar())!=EOF&&c!='\n';len++);
+		if(len>max)max=len;
+	}
+	printf("%d\n",max);
+}
