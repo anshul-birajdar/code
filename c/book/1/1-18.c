fbcba28546a8ee01f147ea9409d837bf50f4e746
anshul-birajdar <linux.forall.yt@gmail.com>
2022-10-11 17:02:39 +0530

    autoupdate 2022-10-11-17:02:39

--- c/book/1/1-18.c
+++ c/book/1/1-18.c
@@ -1,36 +1,27 @@
 #include<stdio.h>
 #define MAXLINE 1000
-int get_line(char *line,int maxline);
-void copy(char *to,char *from);
-/* print longest input line */
+int get_line(char *line,int maxline,char *last);
+/* removes trailing blanks and tabs and prints lines */
 int main(void)
 {
-	int len,max;
-	char line[MAXLINE],longest[MAXLINE];
-	max=0;
-	while((len=get_line(line,MAXLINE))>0) {
-		if(len>max) {
-			max=len;
-			copy(longest,line);
-		}
+	int len;
+	char line[MAXLINE], *end;
+	while((len=get_line(line,MAXLINE,end))>0) {
+		while(*end==' '||*end=='\t')end--;
+		end[1]='\0';
+		printf("%s\n",line);
 	}
-	if(max>0)
-		printf("%s",longest);
 }
-int get_line(char *s,int lim)
+int get_line(char *s,int lim,char *end)
 {
 	int c,i;
 	for(i=0;i<lim-1&&(c=getchar())!=EOF&&c!='\n';i++)
 		s[i]=c;
 	if(c=='\n') {
 		s[i]=c;
+		end=&s[i-1];
 		i++;
 	}
 	s[i]='\0';
 	return i;
-}
-void copy(char *to,char *from)
-{
-	int i=0;
-	while((to[i]=from[i])!='\0')i++;
 }
