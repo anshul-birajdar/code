339a9d31d13783eee46a8ed5dbad368f6573db2c
anshul-birajdar <linux.forall.yt@gmail.com>
2022-10-13 15:37:01 +0530

    autoupdate 2022-10-13-15:37:01

--- c/book/1/1-19.c
+++ c/book/1/1-19.c
@@ -14,7 +14,7 @@ int main(void)
 int get_line(char *s,int lim)
 {
 	int c,i;
-	for(i=0;i<lim-1&&(c=getchar())!=EOF&&c!='\n';i++)
+	for(i=0;i<lim-1&&(c=getc(stdin))!=EOF&&c!='\n';i++)
 		s[i]=c;
 	//if(c=='\n') {
 	//	s[i]=c;
