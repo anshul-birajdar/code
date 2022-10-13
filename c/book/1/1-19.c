6da21561b871e31413a57d8538c85ea963fe220e
anshul-birajdar <linux.forall.yt@gmail.com>
2022-10-13 15:19:01 +0530

    autoupdate 2022-10-13-15:19:01

--- c/book/1/1-19.c
+++ c/book/1/1-19.c
@@ -1,28 +1,32 @@
 #include<stdio.h>
 #define MAXLINE 1000
-char *get_line(char *line,int maxline);
-/* reverses the output, a line at a time */
+int get_line(char *line,int maxline);
+void copy(char *to,char *from);
+/* print input lines with length > 80 */
 int main(void)
 {
-	char line[MAXLINE], *end;
-	while(*(end=get_line(line,MAXLINE))!=EOF) {
-		while(*end==' '||*end=='\t')end--;
-		end[1]='\0';
-		printf("%s",line);
-		if(*line!='\0')printf("\n");
+	int len;
+	char line[MAXLINE];
+	while((len=get_line(line,MAXLINE))>0) {
+		if(len>80) {
+			printf("%s",line);
+		}
 	}
 }
-char *get_line(char *s,int lim)
+int get_line(char *s,int lim)
 {
-	char *end;
 	int c,i;
 	for(i=0;i<lim-1&&(c=getchar())!=EOF&&c!='\n';i++)
 		s[i]=c;
 	if(c=='\n') {
 		s[i]=c;
-		end=&s[i-1];
 		i++;
 	}
 	s[i]='\0';
-	return end;
+	return i;
+}
+void copy(char *to,char *from)
+{
+	int i=0;
+	while((to[i]=from[i])!='\0')i++;
 }
