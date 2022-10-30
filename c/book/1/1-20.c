0d53e815bd89e8a68348be2efb6296e1bab9ea79
anshul-birajdar <linux.forall.yt@gmail.com>
2022-10-30 16:39:57 +0530

    autoupdate 2022-10-30-16:39:57

---
+++ c/book/1/1-20.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#define MAXLINE 1000
+int get_line(char *line,int maxline);
+void reverse(char *to,char *from,int len);
+/* reverses input per line */
+int main(void)
+{
+	char line[MAXLINE];
+	char rev[MAXLINE];
+	int len;
+	while((len=get_line(line,MAXLINE))&&!feof(stdin)) {
+		reverse(rev,line,len);
+		printf("%s\n",rev);
+	}
+}
+int get_line(char *s,int lim)
+{
+	int c,i;
+	for(i=0;i<lim-1&&(c=getchar())!=EOF&&c!='\n';i++)
+		s[i]=c;
+	//if(c=='\n') {
+	//	s[i]=c;
+	//i++;
+	//}
+	s[i]='\0';
+	return i;
+}
+void reverse(char *to,char *from,int len)
+{
+	int i,j;
+	i=j=0;
+	for(i=len-1,j=0;i>=0;i--,j++)
+		to[j]=from[i];
+	to[j]='\0';	
+}
