10a3273fe9a4b58f8440f96da2e64706d3a12e70
anshul-birajdar <linux.forall.yt@gmail.com>
2022-10-30 17:32:01 +0530

    autoupdate 2022-10-30-17:32:01

--- c/book/1/1-11.c
+++ c/book/1/1-11.c
@@ -3,8 +3,7 @@
 #define OUT 0
 /* miniwc -- always prints l w c, ignores options */
 /* test -- ASCII only. Give random input and
-   compare the same input with the program and wc. Newline
-   after a space may be counted as a word */
+   compare the same input with the program and wc. */
 int main(void)
 {
 	int c,nl,nw,nc,state;
