e2d4965ab183cad1aabb526e0efca41adfd24862
anshul-birajdar <linux.forall.yt@gmail.com>
2022-11-08 20:50:02 +0530

    autoupdate 2022-11-08-20:50:01

--- c/book/1/1-21-entab.c
+++ c/book/1/1-21-entab.c
@@ -4,25 +4,31 @@
    and spaces (priority to spaces) */
 int main(void)
 {
-	char c;
-	int len=0;
-	int ts;
-	while((c=getchar())!=EOF) {
-		if(c=='\n') {
-			len=0;
-			putchar('\n');
-		}
-		else if(c=='\t') {
-			ts=len%TABLEN;
-			ts=TABLEN-ts;
-			for(int i=0;i<ts;i++) {
+	char c,pc;
+	c=pc='\0';
+	int spac=0;
+	int len;
+	while(pc=c;(c=getchar())!=EOF) {
+		if(pc==' '&&c!=' ') {
+			while(spac>=TABLEN||spac>=(TABLEN-len%TABLEN)) {
+				putchar('\t');
+				spac-=TABLEN-(len%TABLEN);
+				len+=TABLEN-(len%TABLEN);
+			}
+			for(int i=0;i<spac;i++) {
 				putchar(' ');
 				len++;
 			}
-		}
-		else {
-			len++;
 			putchar(c);
-		}
+			len++;
+			spac=0;
+			continue;
+		
+			if(c==' ')spac++;
+			else {
+				putchar(c);
+				len++;
+			}
+		}	
 	}
 }
